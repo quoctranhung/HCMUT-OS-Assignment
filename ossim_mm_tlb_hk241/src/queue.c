f6e769911a4cfed89c1a4138a62a744666de712c
KeHamTruyen <146626863+KeHamTruyen@users.noreply.github.com>
2024-05-09 23:11:41 +0700

    Update queue.c

--- ossim_mm_tlb_hk241/src/queue.c
+++ ossim_mm_tlb_hk241/src/queue.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 #include "queue.h"
 
@@ -9,12 +9,41 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+	if (q->size == MAX_QUEUE_SIZE) return;
+	q->proc[q->size] = proc;
+	q->size++;
+        
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
-        /* TODO: return a pcb whose prioprity is the highest
-         * in the queue [q] and remember to remove it from q
-         * */
-	return NULL;
+	#ifdef MLQ_SCHED
+	// Return process at index = 0;
+	if (empty(q)) return NULL;
+	struct pcb_t * proc = q->proc[0];
+	for (int i = 0; i < q->size - 1; i++) {
+		q->proc[i] = q->proc[i + 1];
+	}
+	q->size--;
+	q->slot--;
+	return proc;
+	#else
+	/* TODO: return a pcb whose prioprity is the highest
+    * in the queue [q] and remember to remove it from q
+    * */
+	if (empty(q)) return NULL;
+	int min_priority = q->proc[0]->priority;
+	int rt_index = 0;
+	for (int i = 1; i < q->size; i++) {
+		if (q->proc[i]->priority < min_priority) {
+			min_priority = q->proc[i]->priority;
+			rt_index = i;
+		}
+	}
+	for (int i = rt_index; i < q->size - 1; i++) {
+		q->proc[i] = q->proc[i + 1];
+	}
+	q->size--;
+	return q->proc[rt_index];
+	#endif
 }
 
